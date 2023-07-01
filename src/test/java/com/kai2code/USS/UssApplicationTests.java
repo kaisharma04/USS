fcdabd85fc908a61393518002a2808ed4a71f944
kaisharma04 <krishanu.sharma619@gmail.com>
2023-07-01 18:40:03 +0530

    added new files

---
+++ src/test/java/com/kai2code/USS/UssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kai2code.USS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
